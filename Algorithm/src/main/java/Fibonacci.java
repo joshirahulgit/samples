c276ca733f18ebdb3b58a2406e965edcff2cb23e
rahul <joshirahul21@gmail.com>
2017-02-07 21:08:19 -0600

    Fibonacci implemented

--- Algorithm/src/main/java/Fibonacci.java
+++ Algorithm/src/main/java/Fibonacci.java
@@ -4,10 +4,10 @@
 public class Fibonacci {
 
     public static void main(String[] args) {
-        System.out.println(fib(30));
+        System.out.println(fib(10));
     }
 
-    private static int fib(int n) {
+    private static double fib(double n) {
         if (n == 0 || n == 1)
             return n;
         return fib(n - 1) + fib(n - 2);
